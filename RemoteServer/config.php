7bab9d3dcdd92557dae01760d46a2c13c92abd14
yuzijie <git@yuz.me>
2013-08-28 00:58:53 +0200

    remove mysql configuration information, add your own!

--- RemoteServer/config.php
+++ RemoteServer/config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_USER', "yuz"); // db user
-define('DB_PASSWORD', "S36u042y9q37"); // db password
-define('DB_DATABASE', "sync"); // database name
-define('DB_SERVER', "androidproject.db"); // db server
+define('DB_USER', ""); // db user
+define('DB_PASSWORD', ""); // db password
+define('DB_DATABASE', ""); // database name
+define('DB_SERVER', ""); // db server
 ?>
